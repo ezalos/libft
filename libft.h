65ff032892e994c4020e7ac443606acc03dc8fb3
Louis DEVELLE <ldevelle@e2r4p3.42.fr>
2019-02-15 14:02:26 +0100

    Graph Theory

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ldevelle <ldevelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 11:15:02 by ldevelle          #+#    #+#             */
-/*   Updated: 2019/02/14 16:29:11 by ldevelle         ###   ########.fr       */
+/*   Updated: 2019/02/15 13:43:44 by ldevelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,24 @@ typedef	struct		s_list
 }					t_list;
 
 # define BUFF_SIZE 256
+
+#  define C_RED			write(1, "\x1b[31m", 5);
+#  define C_GREEN		write(1, "\x1b[32m", 5);
+#  define C_BLUE		write(1, "\x1b[34m", 5);
+#  define C_YELLOW		write(1, "\x1b[33m", 5);
+#  define C_BLUE		write(1, "\x1b[34m", 5);
+#  define C_MAGENTA		write(1, "\x1b[35m", 5);
+#  define C_CYAN		write(1, "\x1b[36m", 5);
+#  define C_RESET		write(1, "\x1b[0m", 5);
+#  define C_BROWN		write(1, "\x1b[38;2;238;205;163m", 19);
+#  define C_PINK		write(1, "\x1b[38;2;239;100;100m", 19);
+#  define C_PURPLE		write(1, "\x1b[38;2;101;78;163m", 18);
+#  define C_ORANGE		write(1, "\x1b[38;2;155;75;43m", 17);
+#  define C_TURQUOISE	write(1, "\x1b[38;2;68;140;121m", 18);
+#  define CURSOR_RESET	write(1, "\033[0;0H", 6);
+#  define CURSOR_LOAD	write(1, "\033[u", 3);
+#  define CURSOR_SAVE	write(1, "\033[s", 3);
+#  define CLEAR_SCREEN	write(1, "\033[2J", 4);
 
 int					get_next_line(const int fd, char **line);
 

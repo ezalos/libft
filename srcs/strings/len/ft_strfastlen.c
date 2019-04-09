5a6785d0905c3fa0ff5366114bfd6fae1262024d
Louis DEVELLE <ldevelle@e3r4p12.42.fr>
2019-04-09 20:43:23 +0200

    Makefile automated push

--- srcs/strings/len/ft_strfastlen.c
+++ srcs/strings/len/ft_strfastlen.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen2.c                                       :+:      :+:    :+:   */
+/*   ft_strfastlen.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldevelle <ldevelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 11:30:13 by ldevelle          #+#    #+#             */
-/*   Updated: 2019/03/22 16:21:15 by ldevelle         ###   ########.fr       */
+/*   Updated: 2019/04/08 17:04:43 by ldevelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -139,3 +139,42 @@ size_t		ft_strfastlen(const char *str)
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+/*
+** size_t    ft_strlen(const char *s)
+** {
+**     long long int    size;
+**
+**     size = 0;
+**     if (!s || s[0] == '\0')
+**         return (0);
+**     while (s[size] != '\0')
+**     {
+**         if (s[size + 1] == '\0' || s[size + 2] == '\0')
+**             return ((s[size + 1] == '\0') ? size + 1 : size + 2);
+**         else if (s[size + 3] == '\0' || s[size + 4] == '\0')
+**             return ((s[size + 3] == '\0') ? size + 3 : size + 4);
+**         else if (s[size + 5] == '\0' || s[size + 6] == '\0')
+**             return ((s[size + 5] == '\0') ? size + 5 : size + 6);
+**         else if (s[size + 7] == '\0' || s[size + 8] == '\0')
+**             return ((s[size + 7] == '\0') ? size + 7 : size + 8);
+**         else if (s[size + 9] == '\0' || s[size + 10] == '\0')
+**             return ((s[size + 9] == '\0') ? size + 9 : size + 10);
+**         else if (s[size + 11] == '\0' || s[size + 12] == '\0')
+**             return ((s[size + 11] == '\0') ? size + 11 : size + 12);
+**         else if (s[size + 13] == '\0' || s[size + 14] == '\0')
+**             return ((s[size + 13] == '\0') ? size + 13 : size + 14);
+**         size += 15;
+**     }
+**     return (size);
+** }
+*/
